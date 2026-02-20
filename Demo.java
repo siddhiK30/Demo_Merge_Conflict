dac1860516c1d1d6e049fc0eafd851a684334fcd
siddhiK30 <kumbharsiddhi30@gmail.com>
2026-02-20 15:50:47 +0530

    change the EMp added getter setter

--- Demo.java
+++ Demo.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Demo{
 public static void main(String[] args) {
     System.out.println("Hellooooo allll");
@@ -10,11 +10,44 @@ public static void main(String[] args) {
 public class Emp{
     private int empid;
     private int role;
-    private int deisg;
+    private int deisgnation;
     
     int sum=10+20;
-    System.out.println(sum);
-    System.out.println("hellllooooo");
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public int getDeisgnation() {
+        return deisgnation;
+    }
+
+    public void setDeisgnation(int deisgnation) {
+        this.deisgnation = deisgnation;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+    
+    System.out.println(sum+deisgnation);
+
 }
 
 public class  Student {
